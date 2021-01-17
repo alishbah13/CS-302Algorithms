dc91711e3b50470d37a7d219e0eeb084bcfe12b9
alishbah13 <alishbah13@gmail.com>
2021-01-17 16:33:08 +0500

    cleanup

--- app/routes.py
+++ app/routes.py
@@ -2,29 +2,29 @@ from app import app
 from flask import render_template, url_for, request, Flask, flash
 from app.util import getFiles, displayOutput
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
+@app.route('/home', methods=['GET', 'POST'])
+def home():
     return render_template('index.html')
 
 
 @app.route('/lcs', methods=['GET', 'POST'])
 def lcs():
     input_list = getFiles("lcs")
-
+    out = " "
     if request.method == 'POST':
         dat = request.form['file']
-        displayOutput(dat)
-        return render_template('index.html')
-    return render_template('lcs.html', title='Lowest Common Subsequence', inpt=input_list)
+        out = displayOutput(dat)
+        return render_template('lcs.html', title='Lowest Common Subsequence', inpt=input_list , output=out)
+    return render_template('lcs.html', title='Lowest Common Subsequence', inpt=input_list , output=out)
 
 
 @app.route('/scs', methods=['GET', 'POST'])
 def scs():
     input_list = getFiles("scs")
-    
+    out = " "
     if request.method == 'POST':
         dat = request.form['file']
         return dat
     else:
         flash("hello")
-    return render_template('lcs.html', title='Shortest Common Supersequence', inpt=input_list)
+    return render_template('lcs.html', title='Shortest Common Supersequence', inpt=input_list, output=out)
